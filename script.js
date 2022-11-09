36b3b7a9d8eaba95b6b25ddee83c38f133160654
moa-andersson <moaandersson97@hotmail.com>
2022-11-09 15:40:22 +0100

    Java lektion 3

--- script.js
+++ script.js
@@ -1,43 +1,69 @@
 'use strict';
-/* Vad ska vi göra här */
-console.log('test');
 
-const searchInput = null;
+
 const bookList = [
-  {
-    id: 1, 
-    author: 'Charles Dickens',
-    title: 'Oliver Twist'
-  },
-  {
-    id: 2,
-    author: 'William Shakespear',
-    title: 'Hamlet'
-  }
-];
-function handleKeyPress(input) {
-    //ta emot eller läsa av värdet i input-fältet. *
-    // Skicka värdet till searchBooks
-    // SearchBooks returnerar en filtrerad lista 
-    //Filtrerade listan skickas till renderlist
-    searchBooks(input);
-
-}
-
-function searchBooks(searchTerm) {
-    // loopa igenom bookList
-    //För varje varv i loopen kolla på aktuell bok
-    //Och jämför titeln med searchTerm 
-    //Om söktermen finns nånstans i titeln, lägg till boken i en ny lista (filteredList)
-    //Returnerar filteredList eller anropar renderBookList
-for (let i = 0; i < bookList.length; i++){
-    const title = bookList[i].title.toLocaleLowerCase();
-    if(title.indexOf(searchTerm.toLocaleLowerCase()) => 0) {
-
+    {
+        id: 1,
+        author: "Charles Dickens",
+        title: "Oliver Twist"
+    },
+    {
+        id: 2,
+        author: "William Shakespear",
+        title: "Hamlet"
     }
-}
-}
+];
+//const searchInput = document.children[0].children[1].children[1].children[1];//
+const searchField = document.getElementById("searchField");
 
-function renderBookList(list) {
 
+// keydown, keyup
+searchField.addEventListener("keyup", handleKeyPress);
+
+function handleKeyPress(e) {
+    // Ta emot/läsa av värdet i inputfältet.
+    //Skicka värdet till searchBooks
+    //searchBooks returnerar en  filtrerad lista
+    //Filtrera listan skickas till renderBookList/
+    searchBooks(e.target.value);
 }
+    function searchBooks(searchTerm) {
+    //Loopa igenom bookList
+    //För varje varv i loopen, ta det aktuella elementet (boken)
+    //Jänför titel med söktermen
+    //Om söktermen finns någonstans i titeln, lägg till elementeti ny lista(filteredList)
+    //Returnerar filteredList eller anropar RenderBookList*/
+    const filteredList = []
+    for(let i = 0; i < bookList.length; i++) {
+            const title = bookList[i].title.toLowerCase();
+
+            if(title.indexOf(searchTerm.toLowerCase()) >= 0){
+                filteredList.push(bookList[i]);
+            }
+        }
+        renderBookList(filteredList);
+    }
+    function renderBookList(bookList){
+      /* Element i HTML-listan visas/döljs beroende på listans innehåll.*/
+      let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`;
+      for (let i = 0; i < bookList.length; i++) {
+
+        html += `<li
+        class="book-listitem mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b 
+        border-indigo-700 cursor-pointer">
+        ${bookList[i].author} - ${bookList[i].title}
+        </li>`
+
+      }
+      html += `</ul>`;
+
+    const existingElement = document.querySelector('.book-list');
+    console.log(existingElement);
+
+    const root = document.getElementById('root');
+    if (existingElement) {
+      root.removeChild(existingElement);
+    }
+
+    root.insertAdjacentHTML("beforeend", html);
+    }
