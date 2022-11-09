c6aecc53a5e6dc327d512173b71ada3ec0e8dcdd
moa-andersson <moaandersson97@hotmail.com>
2022-11-09 14:31:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 /* Vad ska vi göra här */
 console.log('test');
 
-const searchInput = null;
 const bookList = [
   {
     id: 1, 
@@ -15,12 +14,18 @@ const bookList = [
     title: 'Hamlet'
   }
 ];
-function handleKeyPress(input) {
+const searchField = document.getElementById('searchField');
+console.log(searchField);
+
+//keydown, keyup
+searchField.addEventListener('keyup', handleKeyPress);
+
+function handleKeyPress(e) {
     //ta emot eller läsa av värdet i input-fältet. *
     // Skicka värdet till searchBooks
     // SearchBooks returnerar en filtrerad lista 
     //Filtrerade listan skickas till renderlist
-    searchBooks(input);
+    searchBooks(e.target.value);
 
 }
 
@@ -30,14 +35,19 @@ function searchBooks(searchTerm) {
     //Och jämför titeln med searchTerm 
     //Om söktermen finns nånstans i titeln, lägg till boken i en ny lista (filteredList)
     //Returnerar filteredList eller anropar renderBookList
+    const filteredList = [];
 for (let i = 0; i < bookList.length; i++){
-    const title = bookList[i].title.toLocaleLowerCase();
-    if(title.indexOf(searchTerm.toLocaleLowerCase()) => 0) {
-
+    const title = bookList[i].title.toLowerCase();
+    if(title.indexOf(searchTerm.toLowerCase()) >= 0) {
+      filteredList.push(bookList[i]);
     }
 }
+ console.log(filteredList);
+renderBookList(filteredList);
 }
 
 function renderBookList(list) {
+  //element i html-listan visas eller döljs beroende på listans innehåll
+  //console.log(list)
 
 }
