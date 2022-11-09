8c5bc2d6d695ad27f11cb1d2bd4c6dc72a3c8a97
moa-andersson <moaandersson97@hotmail.com>
2022-11-09 14:54:05 +0100

    mer java script

--- script.js
+++ script.js
@@ -1,53 +1,67 @@
 'use strict';
-/* Vad ska vi göra här */
-console.log('test');
+
 
 const bookList = [
-  {
-    id: 1, 
-    author: 'Charles Dickens',
-    title: 'Oliver Twist'
-  },
-  {
-    id: 2,
-    author: 'William Shakespear',
-    title: 'Hamlet'
-  }
+    {
+        id: 1,
+        author: "Charles Dickens",
+        title: "Oliver Twist"
+    },
+    {
+        id: 2,
+        author: "William Shakespear",
+        title: "Hamlet"
+    }
 ];
-const searchField = document.getElementById('searchField');
-console.log(searchField);
+//const searchInput = document.children[0].children[1].children[1].children[1];//
+const searchField = document.getElementById("searchField");
 
-//keydown, keyup
-searchField.addEventListener('keyup', handleKeyPress);
+
+// keydown, keyup
+searchField.addEventListener("keyup", handleKeyPress);
 
 function handleKeyPress(e) {
-    //ta emot eller läsa av värdet i input-fältet. *
-    // Skicka värdet till searchBooks
-    // SearchBooks returnerar en filtrerad lista 
-    //Filtrerade listan skickas till renderlist
+    // Ta emot/läsa av värdet i inputfältet.
+    //Skicka värdet till searchBooks
+    //searchBooks returnerar en  filtrerad lista
+    //Filtrera listan skickas till renderBookList/
     searchBooks(e.target.value);
-
 }
+    function searchBooks(searchTerm) {
+    //Loopa igenom bookList
+    //För varje varv i loopen, ta det aktuella elementet (boken)
+    //Jänför titel med söktermen
+    //Om söktermen finns någonstans i titeln, lägg till elementeti ny lista(filteredList)
+    //Returnerar filteredList eller anropar RenderBookList*/
+    const filteredList = []
+    for(let i = 0; i < bookList.length; i++) {
+            const title = bookList[i].title.toLowerCase();
 
-function searchBooks(searchTerm) {
-    // loopa igenom bookList
-    //För varje varv i loopen kolla på aktuell bok
-    //Och jämför titeln med searchTerm 
-    //Om söktermen finns nånstans i titeln, lägg till boken i en ny lista (filteredList)
-    //Returnerar filteredList eller anropar renderBookList
-    const filteredList = [];
-for (let i = 0; i < bookList.length; i++){
-    const title = bookList[i].title.toLowerCase();
-    if(title.indexOf(searchTerm.toLowerCase()) >= 0) {
-      filteredList.push(bookList[i]);
+            if(title.indexOf(searchTerm.toLowerCase()) >= 0){
+                filteredList.push(bookList[i]);
+            }
+        }
+        renderBookList(filteredList);
     }
-}
- console.log(filteredList);
-renderBookList(filteredList);
-}
+    function renderBookList(list){
+      /* Element i HTML-listan visas/döljs beroende på listans innehåll.*/
+      let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">
+                  <li
+                      class="book-listitem mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
+                  1
+                  </li>
+                  <li
+                      class="book-listitem mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
+                  2
+                  </li>
+                  </ul>`;
+  const existingElement = document.querySelector(".book-list");
+  console.log(existingElement);
 
-function renderBookList(list) {
-  //element i html-listan visas eller döljs beroende på listans innehåll
-  //console.log(list))))) 
+  const root = document.getElementById("root");
+  if (existingElement){
+      root.removeChild(existingElement);
+  }
 
-}
+  root.insertAdjacentHTML("beforeend", html);
+  }
